8f74fb46b12c9b5cdf774db4581056bbef80869c
Okwir Francis <48248210+OkwirFrances@users.noreply.github.com>
2025-07-26 10:18:58 +0300

    Update plugin_integration_test.dart

--- integration_test/plugin_integration_test.dart
+++ integration_test/plugin_integration_test.dart
@@ -1,4 +1,4 @@
-//import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:hand_landmarker/hand_landmarker.dart';
 import 'package:integration_test/integration_test.dart';
 

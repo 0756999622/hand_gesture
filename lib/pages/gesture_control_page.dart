2b37bb12379579499533897c4338a8d35a8db464
Nantale Cissy <cssnantale@gmail.com>
2025-07-28 11:56:54 +0300

    Made some changes

--- lib/pages/gesture_control_page.dart
+++ lib/pages/gesture_control_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:isolate';
+import 'dart:math';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
